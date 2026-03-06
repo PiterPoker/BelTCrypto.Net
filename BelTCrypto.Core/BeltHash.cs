51e73b996e5e2fd33fcc04111255171c5e3a5fee
PiterPoker <tema5286@gmail.com>
2026-03-06 13:32:44 -0800

    feat: Adaptation of the encryption algorithm to simple substitution mode

--- BelTCrypto.Core/BeltHash.cs
+++ BelTCrypto.Core/BeltHash.cs
@@ -11,7 +11,8 @@ public static class BeltHash
     public static IBelTWideBlock BelTWideBlock(IBelTBlock block) => new BelTWideBlock(block);
     public static BelTKeyWrap BelTKeyWrap(IBelTWideBlock wideBlock) => new(wideBlock);
     public static IBelTCompress BelTCompress(IBelTBlock block) => new BelTCompress(block);
-    public static IBelTEcb BelTEcb(IBelTBlock block) => new BelTEcb(block);
+    public static IBelTEcbTransform BelTEcbEncryptTransform(IBelTBlock block) => new BelTEcbEncryptTransform(block);
+    public static IBelTEcbTransform BelTEcbDecryptTransform(IBelTBlock block) => new BelTEcbDecryptTransform(block);
     public static IBelTCbcTransform BelTCbcEncryptTransform(IBelTBlock block, ReadOnlySpan<byte> iv) => new BelTCbcEncryptTransform(block, iv);
     public static IBelTCbcTransform BelTCbcDecryptTransform(IBelTBlock block, ReadOnlySpan<byte> iv) => new BelTCbcDecryptTransform(block, iv);
 }
--- BelTCrypto.Core/Interfaces/IBelTEcb.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace BelTCrypto.Core.Interfaces;
-
-public interface IBelTEcb : IEncryptor, IDecryptor, IDisposable
-{
-}
