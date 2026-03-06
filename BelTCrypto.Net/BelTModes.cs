d51cbb339d36effa7917960f0a1a202d103030b6
PiterPoker <tema5286@gmail.com>
2026-03-06 15:20:06 -0800

    feat: Implementation of encryption in counter (CTR) mode

---
+++ BelTCrypto.Net/BelTModes.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography;
+
+namespace BelTCrypto.Net;
+
+public static class BelTModes
+{
+    // Используем 6, так как 1-5 уже заняты в .NET
+    public const CipherMode CTR = (CipherMode)6;
+}
