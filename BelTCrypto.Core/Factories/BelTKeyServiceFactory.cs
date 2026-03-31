ad619b1a7bf1d4b944bbb9ea3346f93dbbdbd6d4
PiterPoker <tema5286@gmail.com>
2026-03-31 13:42:01 -0700

    feat: implement key transformation services (8.1, 8.2)

---
+++ BelTCrypto.Core/Factories/BelTKeyServiceFactory.cs
@@ -0,0 +1,8 @@
+﻿using BelTCrypto.Core.Interfaces;
+
+namespace BelTCrypto.Core.Factories;
+
+public static class BelTKeyServiceFactory
+{
+    public static IBelTKeyService Create() => new BelTKeyService(BelTCompressorFactory.Create());
+}
